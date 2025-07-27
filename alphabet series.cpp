430859a7e07415459c80d8769915c557ae9ab72e
TripatjotSingh19 <tripatjots646@gmail.com>
2025-07-27 16:49:25 +0530

    Create alphabet series.cpp

---
+++ alphabet series.cpp
@@ -0,0 +1,28 @@
+/* Tripatjot singh
+PRN 24070123044
+ENTC a2 */
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i ,j;
+    char ch='A';
+    int n=4;
+    for(i=1;i<=n;i++)
+        {
+        for(int j=1;j<=i;j++){
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+}
+}
+
+
+/*
+Output for this code :
+A
+BC
+DEF
+GHIJ
+*/
