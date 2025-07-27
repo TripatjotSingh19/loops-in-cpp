037d2f7146cb4216cca77cbe08a29a9e289f7e29
TripatjotSingh19 <tripatjots646@gmail.com>
2025-07-27 16:44:17 +0530

    Create looping a word.cpp

---
+++ looping a word.cpp
@@ -0,0 +1,25 @@
+/* Tripatjot Singh
+PRN 24070123044
+ENTC A2 */
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1; i<6; i++)
+    {
+        cout<<"SIT\n";
+    }
+}
+
+/* Output for this code
+
+SIT
+SIT
+SIT
+SIT
+SIT
+
+*/
